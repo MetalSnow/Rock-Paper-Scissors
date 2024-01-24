97b560aa00732f08166f2e80e198d13f6792af0f
MetalSnow <yasuotrend@gmail.com>
2024-01-24 12:51:12 +0800

    Implement playRound function for rock-paper-scissors game

--- main.js
+++ main.js
@@ -5,3 +5,27 @@ function getComputerChoice() {
   let randomEl = array[Math.floor(Math.random() * array.length)];
   return randomEl;
 }
+
+// Create a function to determine the result of a round based on player and computer selections
+// The function returns the result
+function playRound(playerSelection, computerSelection) {
+  let result;
+
+  if (playerSelection === computerSelection) {
+    result = "It's a tie!";
+  } else if (
+    (playerSelection === "paper" && computerSelection === "rock") ||
+    (playerSelection === "rock" && computerSelection === "scissors") ||
+    (playerSelection === "scissors" && computerSelection === "paper")
+  ) {
+    result = `You won! ${playerSelection} beats ${computerSelection}.`;
+  } else {
+    result = `You lose! ${computerSelection} beats ${playerSelection}.`;
+  }
+
+  return result;
+}
+
+const playerSelection = "rock".toLowerCase();
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
