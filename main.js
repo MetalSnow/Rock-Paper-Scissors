4e7154c88c1e3e57173b7e0d9297e0411b1c0cde
MetalSnow <yasuotrend@gmail.com>
2024-01-28 12:51:23 +0800

    Update results container's childs and Clear resultsContainer content in playRound function

--- main.js
+++ main.js
@@ -4,8 +4,7 @@ function getComputerChoice() {
   return randomEl;
 }
 
-let playerDiv = document.querySelector(".player");
-let computerDiv = document.querySelector(".computer");
+let resultsContainer = document.querySelector(".results");
 
 function playRound(playerSelection, computerSelection) {
   let result;
@@ -22,22 +21,66 @@ function playRound(playerSelection, computerSelection) {
     result = `You lose! ${computerSelection} beats ${playerSelection}.`;
   }
 
-  DeleteContent();
+  resultsContainer.innerHTML = "";
 
-  let divResults = document.createElement("div");
-  let playerSlected = document.createElement("h3");
-  let computerSlected = document.createElement("h3");
+  let playerSlected = document.createElement("div");
+  let computerSlected = document.createElement("div");
+  let roundResults = document.createElement("div");
 
   playerSlected.textContent = `Player selected: ${playerSelection}`;
-  computerSlected.textContent = `Player selected: ${computerSelection}`;
-  divResults.textContent = result;
+  computerSlected.textContent = `Computer selected: ${computerSelection}`;
+  roundResults.textContent = result;
 
-  playerDiv.appendChild(playerSlected);
-  computerDiv.appendChild(computerSlected);
-  document.body.appendChild(divResults);
+  resultsContainer.appendChild(playerSlected);
+  resultsContainer.appendChild(computerSlected);
+  resultsContainer.appendChild(roundResults);
 }
 
-function DeleteContent() {}
+// function game() {
+//   const totalRounds = 5;
+//   let playerScore = 0;
+//   let computerScore = 0;
+
+//   // Loop through rounds
+//   for (let i = 1; i <= totalRounds; i++) {
+//     const playerSelection = prompt("Choose Rock, Paper or Scissors");
+//     const computerSelection = getComputerChoice();
+//     let roundResult = playRound(
+//       playerSelection.toLowerCase(),
+//       computerSelection
+//     );
+//     console.log("Round: ", i);
+//     console.log(roundResult);
+
+//     // Update scores based on the result of the round
+//     if (roundResult.charAt(4) === "w") {
+//       console.log(
+//         `Score: player ${++playerScore} -- computer ${computerScore}`
+//       );
+//     } else if (roundResult.charAt(4) === "l") {
+//       console.log(
+//         `Score: player ${playerScore} -- computer ${++computerScore}`
+//       );
+//     } else {
+//       console.log(`Score: player ${playerScore} -- computer ${computerScore}`);
+//     }
+
+//     // Determine the winner based on the final scores
+//     if (playerScore === 3 || (i === 5 && playerScore > computerScore)) {
+//       console.log("Victory: You Won!!");
+//       break;
+//     } else if (
+//       computerScore === 3 ||
+//       (i === 5 && playerScore < computerScore)
+//     ) {
+//       console.log("Defeat: You Lost!!");
+//       break;
+//     } else if (i === 5 && playerScore === computerScore) {
+//       console.log("There's No Winner.");
+//     }
+//   }
+// }
+// game();
 
 //Get Buttons
 let rock = document.querySelector("#rockBtn");
