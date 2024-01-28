5a4f6e78de1d8a9e8e1e36eff353fd062d550f54
MetalSnow <yasuotrend@gmail.com>
2024-01-28 11:25:06 +0800

    Update buttons selectors and event listeners

--- main.js
+++ main.js
@@ -18,61 +18,27 @@ function playRound(playerSelection, computerSelection) {
   } else {
     result = `You lose! ${computerSelection} beats ${playerSelection}.`;
   }
-
-  let divResults = document.createElement("div");
-  divResults.textContent = result;
+  console.log(`Player selected: ${playerSelection}`);
   console.log(result);
+  // let divResults = document.createElement("div");
+  // divResults.textContent = result;
+  // document.body.appendChild(divResults);
 }
 
-// Create buttons
-let btnOne = document.createElement("button");
-let btnTwo = document.createElement("button");
-let btnThree = document.createElement("button");
+//Get Buttons
+let rock = document.querySelector("#rockBtn");
+let paper = document.querySelector("#paperBtn");
+let scissors = document.querySelector("#scissorsBtn");
 
-btnOne.addEventListener("click", playRound);
+//Add Event Listeners
+rock.addEventListener("click", () => {
+  playRound("rock", getComputerChoice());
+});
 
-// function game() {
-//   const totalRounds = 5;
-//   let playerScore = 0;
-//   let computerScore = 0;
+paper.addEventListener("click", () => {
+  playRound("paper", getComputerChoice());
+});
 
-//   // Loop through rounds
-//   for (let i = 1; i <= totalRounds; i++) {
-//     const playerSelection = prompt("Choose Rock, Paper or Scissors");
-//     const computerSelection = getComputerChoice();
-//     let roundResult = playRound(
-//       playerSelection.toLowerCase(),
-//       computerSelection
-//     );
-//     console.log("Round: ", i);
-//     console.log(roundResult);
-
-//     // Update scores based on the result of the round
-//     if (roundResult.charAt(4) === "w") {
-//       console.log(
-//         `Score: player ${++playerScore} -- computer ${computerScore}`
-//       );
-//     } else if (roundResult.charAt(4) === "l") {
-//       console.log(
-//         `Score: player ${playerScore} -- computer ${++computerScore}`
-//       );
-//     } else {
-//       console.log(`Score: player ${playerScore} -- computer ${computerScore}`);
-//     }
-
-//     // Determine the winner based on the final scores
-//     if (playerScore === 3 || (i === 5 && playerScore > computerScore)) {
-//       console.log("Victory: You Won!!");
-//       break;
-//     } else if (
-//       computerScore === 3 ||
-//       (i === 5 && playerScore < computerScore)
-//     ) {
-//       console.log("Defeat: You Lost!!");
-//       break;
-//     } else if (i === 5 && playerScore === computerScore) {
-//       console.log("There's No Winner.");
-//     }
-//   }
-// }
-// game();
+scissors.addEventListener("click", () => {
+  playRound("scissors", getComputerChoice());
+});
